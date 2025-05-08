5f88cfd03351f09705f4b7337ecd9a392fafd601
Drogaria Cidade <informatica@drogcidade.com.br>
2025-05-08 11:55:07 -0300

    updated scrap.py

--- scrap.py
+++ scrap.py
@@ -1,4 +1,5 @@
 import os, time, logging, requests 
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
